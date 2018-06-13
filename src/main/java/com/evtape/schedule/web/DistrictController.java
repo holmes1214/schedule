0e7c7c6afa7847cc89395b72c4bd406c5d277393
lianhai <lianhai1136@126.com>
2018-06-13 12:18:42 +0800

    district

--- src/main/java/com/evtape/schedule/web/DistrictController.java
+++ src/main/java/com/evtape/schedule/web/DistrictController.java
@@ -3,11 +3,7 @@ package com.evtape.schedule.web;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.evtape.schedule.consts.ResponseMeta;
 import com.evtape.schedule.domain.District;
@@ -18,62 +14,61 @@ import com.evtape.schedule.persistent.Repositories;
 /**
  * @author ripper 站区接口,增刪改查
  */
-@Controller
+@RestController
 @RequestMapping("/district")
 public class DistrictController {
-	/**
-	 * district列表
-	 */
-	@ResponseBody
-	@RequestMapping(value = "/list", method = { RequestMethod.GET })
-	public ResponseBundle districtList() {
-		return new ResponseBundle().success(Repositories.districtRepository.findAll());
-	}
+    /**
+     * district列表
+     */
+    @GetMapping
+    public ResponseBundle districtList() {
+        return new ResponseBundle().success(Repositories.districtRepository.findAll());
+    }
 
-	/**
-	 * district增
-	 */
-	@ResponseBody
-	@RequestMapping(value = "/add", method = { RequestMethod.POST })
-	public ResponseBundle addDistrict(@RequestBody District district) {
-		try {
-			Repositories.districtRepository.saveAndFlush(district);
-			return new ResponseBundle().success(Repositories.districtRepository.findAll());
-		} catch (Exception e) {
-			return new ResponseBundle().failure(ResponseMeta.REQUEST_PARAM_INVALID);
-		}
-	}
+    /**
+     * district增
+     */
+    @PostMapping
+    public ResponseBundle addDistrict(@RequestBody District district) {
+        try {
+            Repositories.districtRepository.saveAndFlush(district);
+            return new ResponseBundle().success(Repositories.districtRepository.findAll());
+        } catch (Exception e) {
+            return new ResponseBundle().failure(ResponseMeta.REQUEST_PARAM_INVALID);
+        }
+    }
 
-	/**
-	 * district改
-	 */
-	@ResponseBody
-	@RequestMapping(value = "/update", method = { RequestMethod.PUT })
-	public ResponseBundle updateDistrict(@RequestBody District district) {
-		try {
-			Repositories.districtRepository.saveAndFlush(district);
-			return new ResponseBundle().success(Repositories.districtRepository.findAll());
-		} catch (Exception e) {
-			return new ResponseBundle().failure(ResponseMeta.REQUEST_PARAM_INVALID);
-		}
-	}
+    /**
+     * district改
+     */
+    @PutMapping("/{id}")
+    public ResponseBundle updateDistrict(@PathVariable("id") Integer id, @RequestBody District form) {
+        try {
+            District district = Repositories.districtRepository.findOne(id);
+            district.setDistrictName(form.getDistrictName());
+            district.setContent(form.getContent());
+            Repositories.districtRepository.saveAndFlush(district);
+            return new ResponseBundle().success(Repositories.districtRepository.findAll());
+        } catch (Exception e) {
+            return new ResponseBundle().failure(ResponseMeta.REQUEST_PARAM_INVALID);
+        }
+    }
 
-	/**
-	 * district删
-	 */
-	@ResponseBody
-	@RequestMapping(value = "/delete", method = { RequestMethod.DELETE })
-	public ResponseBundle deleteDistrict(@RequestParam("districtId") Integer districtId) {
-		try {
-
-			List<Station> list = Repositories.stationRepository.findByDistrictId(districtId);
-			if (list != null && list.size() > 0) {
-				return new ResponseBundle().failure(ResponseMeta.DISTRICT_HASSTATION);
-			}
-			Repositories.districtRepository.delete(districtId);
-			return new ResponseBundle().success(Repositories.districtRepository.findAll());
-		} catch (Exception e) {
-			return new ResponseBundle().failure(ResponseMeta.REQUEST_PARAM_INVALID);
-		}
-	}
+    /**
+     * district删
+     */
+    @DeleteMapping("/{id}")
+    public ResponseBundle deleteDistrict(@PathVariable("id") Integer districtId) {
+        try {
+            List<Station> list = Repositories.stationRepository.findByDistrictId(districtId);
+            if (list != null && list.size() > 0) {
+                return new ResponseBundle().failure(ResponseMeta.DISTRICT_HASSTATION);
+            }
+            Repositories.districtRepository.delete(districtId);
+            return new ResponseBundle().success(Repositories.districtRepository.findAll());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseBundle().failure(ResponseMeta.REQUEST_PARAM_INVALID);
+        }
+    }
 }
