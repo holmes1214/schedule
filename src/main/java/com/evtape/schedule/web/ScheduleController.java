a7ac8e978b5ab516549f5aedf1124cbcbccdc95a
zouhao <zouh@ducetech.com>
2018-07-16 17:42:37 +0800

    bug

--- src/main/java/com/evtape/schedule/web/ScheduleController.java
+++ src/main/java/com/evtape/schedule/web/ScheduleController.java
@@ -302,7 +302,7 @@ public class ScheduleController {
             Map<Integer, List<ScheduleWorkflow>> workflowMap=workflowList.stream().collect(Collectors.groupingBy(ScheduleWorkflow::getClassId));;
             List<ScheduleWorkflowContent> contentList=Repositories.contentRepository.findByDistrictId(user.getDistrictId());
             Map<Integer, List<ScheduleWorkflowContent>> contentMap=contentList.stream().collect(Collectors.groupingBy(ScheduleWorkflowContent::getWorkFlowId));
-            response.setHeader("Content-Disposition", "attachment; filename=\"排班.zip\"");
+            response.setHeader("Content-Disposition", "attachment; filename=\"schedule.zip\"");
             response.setContentType("application/octet-stream;charset=UTF-8");
             PictureUtil.createUserSchedulePicture(scheduleMap,shiftMap,workflowMap,contentMap,response.getOutputStream());
         } catch (Exception e) {
