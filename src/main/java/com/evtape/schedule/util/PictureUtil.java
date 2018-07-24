5753f53d043f949582670fd9d98f8f875d0f54b2
zouhao <zouh@ducetech.com>
2018-07-24 17:49:36 +0800

    bug

--- src/main/java/com/evtape/schedule/util/PictureUtil.java
+++ src/main/java/com/evtape/schedule/util/PictureUtil.java
@@ -25,7 +25,7 @@ public class PictureUtil {
     static final int MARGIN = 80;
     static final int SPACE_SIZE_BASE = 40;
     static final int FONT_SIZE_BASE = 32;
-    static final int HOUR_BASE = SPACE_SIZE_BASE * 4;
+    static final int HOUR_BASE = SPACE_SIZE_BASE * 6;
     static final int[] VERTICALS = new int[]{MARGIN, SPACE_SIZE_BASE * 10, SPACE_SIZE_BASE * 3, SPACE_SIZE_BASE * 12, SPACE_SIZE_BASE * 8, SPACE_SIZE_BASE * 8, HOUR_BASE * 24};
     static final String[] TITLES = new String[]{"日期", "星期", "工作时间", "班次", "工作岗位"};
     static final String[] PERIODS = new String[]{"0100", "0200", "0300", "0400", "0500", "0600", "0700", "0800", "0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300", "0000"};
@@ -121,7 +121,7 @@ public class PictureUtil {
             graphics.drawLine(x, top, x, top + SPACE_SIZE_BASE);
         }
         //时间表分钟线
-        for (int i = left; i > left - VERTICALS[VERTICALS.length - 1]; i -= SPACE_SIZE_BASE / 2) {
+        for (int i = left; i > left - VERTICALS[VERTICALS.length - 1]; i -= SPACE_SIZE_BASE) {
             graphics.drawLine(i, top + SPACE_SIZE_BASE, i, bottom);
         }
     }
