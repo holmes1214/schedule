6861d8d30ce30520cc271261451f3e608b083219
Sickle2 <1457479761@qq.com>
2018-08-24 14:48:27 +0800

    换班bug修改

--- src/main/java/com/evtape/schedule/web/ScheduleChangeController.java
+++ src/main/java/com/evtape/schedule/web/ScheduleChangeController.java
@@ -3,6 +3,7 @@ package com.evtape.schedule.web;
 import com.evtape.schedule.consts.Constants;
 import com.evtape.schedule.consts.ResponseMeta;
 import com.evtape.schedule.domain.ScheduleInfo;
+import com.evtape.schedule.domain.ScheduleLeave;
 import com.evtape.schedule.domain.User;
 import com.evtape.schedule.domain.vo.ResponseBundle;
 import com.evtape.schedule.persistent.Repositories;
@@ -80,21 +81,23 @@ public class ScheduleChangeController {
             if (changes.size() > 0) {
                 changeList = Repositories.scheduleInfoRepository.findByUserIdsNoEndDate(df.parse(startDateStr), changes.stream().map(User::getId).collect(Collectors.toList()));
             }
+            Repositories.scheduleInfoRepository.delete(userList);
+            Repositories.scheduleInfoRepository.delete(changeList);
             //求循环次数
             int size = userList.size() > changeList.size() ? (userList.size() - changeList.size()) : (changeList.size() - userList.size());
             if (userList.size() > changeList.size()) {
                 ScheduleInfo scheduleInfo = new ScheduleInfo();
-                scheduleInfo.setUserId(users.get(0).getId());
-                scheduleInfo.setUserName(users.get(0).getUserName());
-                scheduleInfo.setVersion(0);
+                scheduleInfo.setUserId(changes.get(0).getId());
+                scheduleInfo.setUserName(changes.get(0).getUserName());
+                scheduleInfo.setModified(0);
                 for (int i = 0; i < size; i++) {
                     changeList.add(scheduleInfo);
                 }
             } else {
                 ScheduleInfo scheduleInfo = new ScheduleInfo();
-                scheduleInfo.setUserId(changes.get(0).getId());
-                scheduleInfo.setUserName(changes.get(0).getUserName());
-                scheduleInfo.setVersion(0);
+                scheduleInfo.setUserId(users.get(0).getId());
+                scheduleInfo.setUserName(users.get(0).getUserName());
+                scheduleInfo.setModified(0);
                 for (int i = 0; i < size; i++) {
                     userList.add(scheduleInfo);
                 }
@@ -106,41 +109,18 @@ public class ScheduleChangeController {
                     BeanUtils.copyProperties(changeList.get(i), change);
                     ScheduleInfo user = new ScheduleInfo();
                     BeanUtils.copyProperties(userList.get(i), user);
-                    user.setDistrictId(changeList.get(i).getDistrictId());
-                    user.setDutyName(changeList.get(i).getDutyName());
-                    user.setDutyCode(changeList.get(i).getDutyCode());
-                    user.setDutyClassId(changeList.get(i).getDutyClassId());
-                    user.setPositionId(changeList.get(i).getPositionId());
-                    user.setPositionName(changeList.get(i).getPositionName());
-                    user.setStationId(changeList.get(i).getStationId());
-                    user.setSuiteId(changeList.get(i).getSuiteId());
-                    user.setWorkflowId(changeList.get(i).getWorkflowId());
-                    user.setWorkingHours(changeList.get(i).getWorkingHours());
-                    user.setWorkflowCode(changeList.get(i).getWorkflowCode());
-                    user.setDateStr(changeList.get(i).getDateStr());
-                    user.setScheduleDate(changeList.get(i).getScheduleDate());
-                    user.setScheduleWeek(changeList.get(i).getScheduleWeek());
+                    user.setUserName(changeList.get(i).getUserName());
+                    user.setUserId(changeList.get(i).getUserId());
+                    user.setModified(changeList.get(i).getModified());
                     if (user.getVersion() == null) {
                         user.setVersion(0);
                     } else {
                         user.setVersion(user.getVersion() + 1);
                     }
 
-
-                    change.setDistrictId(userList.get(i).getDistrictId());
-                    change.setDutyName(userList.get(i).getDutyName());
-                    change.setDutyCode(userList.get(i).getDutyCode());
-                    change.setDutyClassId(userList.get(i).getDutyClassId());
-                    change.setPositionId(userList.get(i).getPositionId());
-                    change.setPositionName(userList.get(i).getPositionName());
-                    change.setStationId(userList.get(i).getStationId());
-                    change.setSuiteId(userList.get(i).getSuiteId());
-                    change.setWorkflowId(userList.get(i).getWorkflowId());
-                    change.setWorkingHours(userList.get(i).getWorkingHours());
-                    change.setWorkflowCode(userList.get(i).getWorkflowCode());
-                    change.setDateStr(userList.get(i).getDateStr());
-                    change.setScheduleDate(userList.get(i).getScheduleDate());
-                    change.setScheduleWeek(userList.get(i).getScheduleWeek());
+                    change.setUserId(userList.get(i).getUserId());
+                    change.setUserName(userList.get(i).getUserName());
+                    change.setModified(userList.get(i).getModified());
                     if (change.getVersion() == null) {
                         change.setVersion(0);
                     } else {
@@ -148,10 +128,20 @@ public class ScheduleChangeController {
                     }
 
                     if (StringUtils.isNotBlank(user.getDateStr())) {
-                        Repositories.scheduleInfoRepository.save(user);
+                        ScheduleInfo scheduleInfo1 = Repositories.scheduleInfoRepository.saveAndFlush(user);
+                        if (scheduleInfo1.getModified() != null && scheduleInfo1.getModified() > 0){
+                            ScheduleLeave leave = Repositories.scheduleLeaveRepository.findByUserIdAndAndLeaveDateStr(scheduleInfo1.getUserId(),scheduleInfo1.getDateStr());
+                            leave.setScheduleInfoId(scheduleInfo1.getId());
+                            Repositories.scheduleLeaveRepository.saveAndFlush(leave);
+                        }
                     }
                     if (StringUtils.isNotBlank(change.getDateStr())) {
-                        Repositories.scheduleInfoRepository.save(change);
+                        ScheduleInfo scheduleInfo1 = Repositories.scheduleInfoRepository.saveAndFlush(change);
+                        if (scheduleInfo1.getModified() != null && scheduleInfo1.getModified() > 0){
+                            ScheduleLeave leave = Repositories.scheduleLeaveRepository.findByUserIdAndAndLeaveDateStr(scheduleInfo1.getUserId(),scheduleInfo1.getDateStr());
+                            leave.setScheduleInfoId(scheduleInfo1.getId());
+                            Repositories.scheduleLeaveRepository.saveAndFlush(leave);
+                        }
                     }
                 }
             }
